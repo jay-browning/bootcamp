3f64dd91fd762bdea7bb918c145925a14e0324d3
JH Browning <ralphkensington@gmail.com>
2023-02-18 09:12:57 +0000

    changed spacy module back to 'en_core_web_md',
    fixed description note

--- helloWorld.py
+++
@@ -1,6 +0,0 @@
-__author__ = "Jay Browning"
-
-# this program will accept a string from a user and print it back out again.  neat
-
-user_input = input(print('enter some text to be printed\n>>> '))
-print(user_input)
