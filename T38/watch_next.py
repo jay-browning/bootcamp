68add15c048767e63d0587146d7445eeaacaaefd
JH Browning <ralphkensington@gmail.com>
2023-02-18 10:44:30 +0000

    edited for formatting

--- T38/watch_next.py
+++ T38/watch_next.py
@@ -9,8 +9,8 @@ nlp = spacy.load('en_core_web_md')
 movies_desc = []
 recc_scores = []
 hulk = '''Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, the Illuminati 
-       'trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, 
-       'Hulk land on the planet Sakaar where he is sold into slavery and trained as a gladiator.'''
+       trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, 
+       Hulk land on the planet Sakaar where he is sold into slavery and trained as a gladiator.'''
 
 # read movies.txt file in to movies_desc, stripping line breaks
 # and splitting into nested lists with 'movie x' and description as separate list items
@@ -22,7 +22,6 @@ with open('./movies.txt', 'r') as db:
 token_ = nlp(hulk)
 for movie in movies_desc:                                   # iterate through list of movies descriptions
     token = nlp(movie[1])                                   # compare only the description, not the 'Movie x' portion
-    #print(f'{token.similarity(token_)} : {token}')
     recc_scores.append([token.similarity(token_), movie])   # run comparison, add to scores list for further analysis
 
 # format and display scores, cause, why not.
